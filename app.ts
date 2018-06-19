cf348f54bc3ee0525d3ba8c8664bb42077bc0a53
JPSSouza <joao_p_souza@live.com>
2018-06-19 15:16:52 -0300

    Type Script configurado no Atom

---
+++ app.ts
@@ -0,0 +1,8 @@
+let message: string = "Obrigado Senhor!";
+console.log(message);
+
+let epsodio: number = 1;
+console.log("Episodio Atual: " + epsodio);
+
+epsodio += "1";
+console.log("Proximo Episodio: " + epsodio );
---
+++ comp/app.js
@@ -0,0 +1,6 @@
+var message = "Obrigado Senhor!";
+console.log(message);
+var epsodio = 1;
+console.log("Episodio Atual: " + epsodio);
+epsodio += "1";
+console.log("Proximo Episodio: " + epsodio);
